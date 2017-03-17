7f2c89999e9f19965f2c4a10e97e560644384fc8
Agata <agata@student.edu>
2017-03-17 12:46:38 +0100

    TAAAAAAAAAAAAAAAAAAAAAAAAAAK

--- final_project/web/app.js
+++ final_project/web/app.js
@@ -2,33 +2,21 @@
 $(document).ready(function(){
     
     $('.quote').on('click', function(event){
-//        var file = document.getElementById("#form_File").textContent;
-//        console.log(file);
-        
+      
         var languagePairs = document.getElementsByName('form[languagePairs]');
-        console.log(languagePairs);
-        console.log('AAA');
-        
+      
         for(i=0; i<languagePairs.length; i++){
-            
-            console.log(languagePairs[i].value);
-        }
-//
-//        for (i = 0; i < languagePairs.length; i++) {  
-//            
-//            console.log(languagePairs[i]);
-////        if((languagePairs[i].type==="radio") && languagePairs[i].checked===true){
-//        if(languagePairs[i].checked){
-//            
+            if(languagePairs[i].checked===true){
+                             
+                var label = languagePairs[i].nextElementSibling;
+                console.log(label);
+                var text = label.innerText;
+                console.log(text);
+                
+            }
 //            console.log(languagePairs[i].value);
-//
-////    }
-//        }
-//        
-//        }
-//    },
-        
-        
+        }
+               
 //        $.ajax({
 //          url: 'numberOfSignsFunction.php?action=extractText',
 //          method: 'post'
