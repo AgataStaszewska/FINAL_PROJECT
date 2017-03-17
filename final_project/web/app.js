ba7d9777aa67fb8ca55c9f513219dcbc1404e317
Agata <agata@student.edu>
2017-03-17 14:26:50 +0100

    MWAHAHAAHAHAHAH

--- final_project/web/app.js
+++ final_project/web/app.js
@@ -4,17 +4,51 @@ $(document).ready(function(){
     $('.quote').on('click', function(event){
       
         var languagePairs = document.getElementsByName('form[languagePairs]');
-      
+        var field = document.getElementsByName('form[field]');
+        var file = document.getElementById('form_File'); //TO JESZCZE NIE DZIAŁA
+        
+        console.log(file.files);
+        
+        if(('files' in file) && (file.files.length!=0)){
+            
+            var name = file.files[0].name;
+            console.log(name);
+            
+        }
+        
+        
+        
+        
+//        for(i=0; i<file.files.length; i++){
+//            
+//            if('name' in file){
+//                
+//                console.log(file.name); //nope
+//                
+//            }
+//            
+//        }
+        
+        
+
         for(i=0; i<languagePairs.length; i++){
             if(languagePairs[i].checked===true){
                              
                 var label = languagePairs[i].nextElementSibling;
-                console.log(label);
                 var text = label.innerText;
                 console.log(text);
                 
             }
-//            console.log(languagePairs[i].value);
+        }
+        
+        for(i=0; i<field.length; i++){
+            if(field[i].checked===true){
+                
+                var label = field[i].nextElementSibling;
+                var text = label.innerText;
+                console.log(text);
+            }
+            
         }
                
 //        $.ajax({
